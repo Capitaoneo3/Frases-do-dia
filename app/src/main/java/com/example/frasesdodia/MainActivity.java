d7b34ad65b2cf23ab084d5a6400086ff5072b5fa
João Gabriel Capitaneo <joaocapitaneo@gmail.com>
2020-10-08 16:46:03 -0300

    comitao

---
+++ app/src/main/java/com/example/frasesdodia/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.frasesdodia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view){
+
+        String[] frases = {"Carla","joji","zequinha"};
+        TextView texto=findViewById(R.id.conteudo_frase);
+        texto.setText("Joao");
+        
+    }
+}
